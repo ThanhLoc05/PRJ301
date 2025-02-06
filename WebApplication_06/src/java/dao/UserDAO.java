0f4d604702334f49cebe85c53db04104a1a2b529
ThanhLoc05 <thanhlocle16052004@gmail.com>
2025-02-06 11:17:15 +0700

    WebApplication_06
    WebApplication_06

--- WebApplication_06/src/java/dao/UserDAO.java
+++ WebApplication_06/src/java/dao/UserDAO.java
@@ -1,14 +1,66 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+DAO ( Data Access Object )
  */
 package dao;
+
+import dto.UserDTO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.DBUtils;
 
 /**
  *
  * @author acer
  */
-public class UserDAO {
+public class UserDAO implements IDAO<UserDTO, String>{
+    
+    @Override
+    public boolean create(UserDTO entity) {
+        String sql = "INSERT [dbo].[tblUsers] ([userID], [fullName], [roleID], [password]) "
+                + "VALUES (N'"+entity.getUserID()
+                +"', N'"+entity.getFullName()
+                +"', N'"+entity.getRoleID()
+                +"', N'"+entity.getPassword()+"')";
+        try {
+            Connection conn = DBUtils.getConnection();
+            Statement st = conn.createStatement();
+            int n = st.executeUpdate(sql);
+            return n>0;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public List<UserDTO> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public UserDTO readById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(UserDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<UserDTO> search(String searchTerm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- WebApplication_06/src/java/models/User.java
+++ WebApplication_06/src/java/models/User.java
@@ -10,5 +10,63 @@ package models;
  * @author acer
  */
 public class User {
+    private String userID;
+    private String fullName;
+    private String adddress;
+    private String password;
+    private String roleID;
+    // .... add more
+
+    public User() {
+    }
+
+    public User(String userID, String fullName, String adddress, String password, String roleID) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.adddress = adddress;
+        this.password = password;
+        this.roleID = roleID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAdddress() {
+        return adddress;
+    }
+
+    public void setAdddress(String adddress) {
+        this.adddress = adddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
     
+    // .... add more
 }
